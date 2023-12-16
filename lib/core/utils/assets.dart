5afdea5a5a181814660088593d69168f1c60cb3e
Ahmedelkazaz39 <Ahmedelkazaz39>
2023-12-16 20:04:44 +0200

    add custom list view item

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,5 +1,5 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
-    static const search = 'assets/images/ic search.png';
-
+  static const search = 'assets/images/ic search.png';
+  static const testImage = 'assets/images/Book 2 Hightligh.png';
 }
