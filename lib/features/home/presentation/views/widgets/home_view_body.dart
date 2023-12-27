6dceb725e51b8d4ad101e0fdf9d8dad6f14cc3c9
Ahmedelkazaz39 <Ahmedelkazaz39>
2023-12-27 07:25:40 +0200

    best seller item is done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,5 @@
-import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/assets.dart';
-import 'package:bookly_app/core/utils/styles.dart';
+  import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
@@ -33,73 +32,3 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 115,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.red,
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AssetsData.testImage,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child:   Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20.copyWith(
-                    fontFamily: kGTSectraFine
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Text(
-                'J.K. Rowling',
-                style: Styles.textStyle14.copyWith(
-                  color: const Color(0xff707070),
-                ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-                Row(
-                children: [
-                  Text(
-                  '19.99 €',
-                  style: Styles.textStyle20.copyWith(
-                    fontWeight: FontWeight.bold
-                  ),                 
-                ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
