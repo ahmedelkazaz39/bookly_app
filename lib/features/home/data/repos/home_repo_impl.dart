446d1df36a184c25979fd874753b756a26e831de
Ahmedelkazaz39 <Ahmedelkazaz39>
2024-02-04 07:16:11 +0200

    impelement newest books

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl({required this.apiService});
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() {
+    apiService.get(
+        endPoint:
+            'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming');
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
